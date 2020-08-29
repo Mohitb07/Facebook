704e4edb0f9e1252f14205dc661430e9a4a28d58
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2020-08-29 22:33:01 +0530

    Implemented Header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAoeuTSiRjNIHlQKC0tkb9SIiAwRtsfMTo",
+    authDomain: "facebook-clone-63584.firebaseapp.com",
+    databaseURL: "https://facebook-clone-63584.firebaseio.com",
+    projectId: "facebook-clone-63584",
+    storageBucket: "facebook-clone-63584.appspot.com",
+    messagingSenderId: "383007834860",
+    appId: "1:383007834860:web:584c43872c7de8eed8dbc9",
+    measurementId: "G-HLJ42Q1PYX"
+  };
